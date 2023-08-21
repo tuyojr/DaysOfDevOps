38d2ff476d5f0db089e3c2a9a68abb7339f1eaed
tuyojr <olutuyod@gmail.com>
2023-08-21 11:29:54 +0100

    first go program

---
+++ day_07_learning_a_programming_language/src/linux/hello.go
@@ -0,0 +1,13 @@
+// the first line in a go file needs to be the name of the package
+// for us to be able to execute it, we need to have a package called main.
+package main
+
+// we can now import different libraries now.
+import (
+	"fmt"
+)
+
+// this is where the program starts
+func main() {
+	fmt.Println("Wagwan peeps!!!")
+}
