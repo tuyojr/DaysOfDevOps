b8a4a9e3a75beb6bc72a70f769fbadf3d169580a
tuyojr <olutuyod@gmail.com>
2023-08-21 11:47:34 +0100

    getting starting with variable declarations and if statements

---
+++ day_07_learning_a_programming_language/src/linux/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// declaring a variable
+	var x int = 5 // x := 5 is another way of declaring a variable
+	var y int = 7
+	var sum int = x + y
+
+	fmt.Println(sum)
+
+	// control statement
+	if sum > 10 {
+		fmt.Println("The sum is greater than 10")
+	} else if sum < 10 {
+		fmt.Println("The sum is less than 10")
+	} else {
+		fmt.Println("The sum is equal to 10")
+	}
+}
