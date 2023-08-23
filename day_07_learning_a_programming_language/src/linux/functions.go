82183de37378342d1ea72d92bfc139235c98c5ff
tuyojr <olutuyod@gmail.com>
2023-08-23 16:51:44 +0100

    creating simple functions to perform basic arithmetic

---
+++ day_07_learning_a_programming_language/src/linux/functions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	result := sum(1, 2)
+	fmt.Println(result)
+
+	// if x is a negative number, sqrt() will return an error
+	sqResult, err := sqrt(-16)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(sqResult)
+	}
+}
+
+// funcKeyWord funcName (paramName paramType, paramName paramType) returnType { code }
+func sum(x int, y int) int {
+	return x + y
+}
+
+// function that has multiple return statements
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("undefined for negative numbers")
+	}
+	return math.Sqrt(x), nil
+}
