69f2614f862f0161350e4f00189548a73c21a6c9
tuyojr <olutuyod@gmail.com>
2023-08-23 14:48:59 +0100

    script to run on startup

---
+++ update_apt_repository_on_start.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+# Update the log file everytime the script is run
+date >> /home/tuyojr/update_logs.txt
+
+# Update the repository information
+sudo apt update
+
+# Check if there are any upgrades available
+if [ $(sudo apt list --upgradable 2>/dev/null | grep -c 'upgradable') -gt 0 ]; then
+    echo "Updates are available. Performing upgrades..."
+    sudo apt dist-upgrade -y
+else
+    echo "No updates available."
+fi
