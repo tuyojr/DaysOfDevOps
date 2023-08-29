507011d25c1f088800a76e7eab47b99250cb385b
tuyojr <olutuyod@gmail.com>
2023-08-29 09:40:47 +0100

    creating an object from a struct

---
+++ day_07_learning_a_programming_language/src/linux/struct.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// a struct is a collection of fields. It's similar to a class in OOP
+
+type person struct {
+	name    string
+	age     int
+	powerUp string
+}
+
+func main() {
+	// create an object of type person
+	u1 := person{"Shisui", 25, "Kotoamatsukami"}
+	fmt.Println(u1)
+
+	// use the dot operator to access the fields of the struct
+	fmt.Println(u1.name)
+}
