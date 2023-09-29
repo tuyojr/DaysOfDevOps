93dca39fb145bc747b62de36a6cdda0a5c9f6621
tuyojr <olutuyod@gmail.com>
2023-09-29 09:25:12 +0100

    renamed package

--- day_07_learning_a_programming_language/src/linux/arrays/arrays.go
+++ day_07_learning_a_programming_language/src/linux/arrays/arrays.go
@@ -1,4 +1,4 @@
-package main
+package arrays
 
 import "fmt"
 
--- day_07_learning_a_programming_language/src/linux/hello/hello.go
+++ day_07_learning_a_programming_language/src/linux/hello/hello.go
@@ -1,6 +1,6 @@
-// the first line in a go file needs to be the name of the package
-// for us to be able to execute it, we need to have a package called main.
-package main
+// Package hello: the first line in a go file needs to be the name of the package
+// for us to be able to execute it, we need to have a package called main/any other name.
+package hello
 
 // we can now import different libraries now.
 import (
