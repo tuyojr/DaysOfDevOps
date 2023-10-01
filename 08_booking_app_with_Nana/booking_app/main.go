86f87f2ab1b488f985551bfbdd9cb401d2e83b6f
tuyojr <olutuyod@gmail.com>
2023-10-01 13:28:28 +0100

    defining user variables

--- 08_booking_app_with_Nana/booking_app/main.go
+++ 08_booking_app_with_Nana/booking_app/main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-    fmt.Println("Welcome to the Muffin Conference.")
-    fmt.Println("You can buy your tickets here.")
+func main() {
+	var conferenceName = "Muffin Conference"
+	const conferenceTickets = 27
+	var remainingTickets = 27
+
+	fmt.Printf("Welcome to the %v booking application.\n", conferenceName)
+	fmt.Printf("There are %v tickets in total, and there are %v tickets still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("You can buy your tickets here to attend.")
+
+	var userName string
+	var userTickets int
 }
