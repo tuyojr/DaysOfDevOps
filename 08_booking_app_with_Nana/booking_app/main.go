bb44c8c5cdbfcb472c0766ac001221185fe690b9
tuyojr <olutuyod@gmail.com>
2023-10-03 11:31:48 +0100

    getting user input to book tickets

--- 08_booking_app_with_Nana/booking_app/main.go
+++ 08_booking_app_with_Nana/booking_app/main.go
@@ -13,16 +13,24 @@ func main() {
 	fmt.Printf("There are %v tickets in total, and there are %v tickets still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("You can buy your tickets here to attend.")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
 	// we use a pointer '&' to get the memory address of the variable, that references the actual value
 	// Scan will read whatever the user enters and assign the value to the variable
-	fmt.Println("Please enter a user name:")
-	fmt.Scan(&userName)
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
 
 	fmt.Println("Please enter how many tickets you want:")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Welcome %v! You have booked %v tickets. Enjoy!\n", userName, userTickets)
+	fmt.Printf("Thank you %v %v! You have successfully bought %v tickets, you should get a confirmation shortly in your email: %v.\n", firstName, lastName, userTickets, email)
 }
