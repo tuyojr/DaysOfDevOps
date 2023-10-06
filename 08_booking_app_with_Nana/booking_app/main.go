6ef5384c4c6c9f9170b3cb2445b23d113a088f96
tuyojr <olutuyod@gmail.com>
2023-10-06 12:36:26 +0100

    prompt user for input continuously

--- 08_booking_app_with_Nana/booking_app/main.go
+++ 08_booking_app_with_Nana/booking_app/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// this alternative syntax cannot be used to declare constants ==> variable := value
@@ -14,32 +17,42 @@ func main() {
 	fmt.Printf("There are %v tickets in total, and there are %v tickets still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("You can buy your tickets here to attend.")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
 
-	// we use a pointer '&' to get the memory address of the variable, that references the actual value
-	// Scan will read whatever the user enters and assign the value to the variable
-	fmt.Println("Please enter your first name:")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Please enter your last name:")
-	fmt.Scan(&lastName)
+		// we use a pointer '&' to get the memory address of the variable, that references the actual value
+		// Scan will read whatever the user enters and assign the value to the variable
+		fmt.Println("Please enter your first name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your email:")
-	fmt.Scan(&email)
+		fmt.Println("Please enter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter how many tickets you want:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Please enter your email:")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
+		fmt.Println("Please enter how many tickets you want:")
+		fmt.Scan(&userTickets)
 
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("Thank you %v %v! You have successfully bought %v tickets, you should get a confirmation shortly in your email: %v.\n", firstName, lastName, userTickets, email)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("There are now %v tickets left.\n", remainingTickets)
+		fmt.Printf("Thank you %v %v! You have successfully bought %v tickets, you should get a confirmation shortly in your email: %v.\n", firstName, lastName, userTickets, email)
 
-	fmt.Printf("These are all the bookings in our conference: %v", bookings)
+		fmt.Printf("There are now %v tickets left.\n", remainingTickets)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings { // _ is a blank identifier, it is a variable we are explicitly not using.
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The people who have booked include: %v\n", firstNames)
+	}
 }
