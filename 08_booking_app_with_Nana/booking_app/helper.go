3b6d7b0d8d51ecc22cf5afd4183342e1d7dcac45
tuyojr <olutuyod@gmail.com>
2023-10-16 11:22:13 +0100

    helper function to validate user input in the main package

---
+++ 08_booking_app_with_Nana/booking_app/helper.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidUserTickets := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidUserTickets
+}
